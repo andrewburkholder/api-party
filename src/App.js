17185dbd9734a13e7752b625191d14c05f927799
Andrew Burkholder <hi@andrew.vip>
2018-07-10 15:51:57 -0400

    add NavLinks and css to App

--- src/App.js
+++ src/App.js
@@ -1,17 +1,30 @@
-import React, { Component } from 'react';
-import './App.css';
+import React, { Component } from 'react'
+import { Route, NavLink, Switch } from 'react-router-dom'
+
+import './App.css'
 
 class App extends Component {
   render() {
     return (
-      <div className='App'>
+      <div className="App">
         <header>
-        <h1>API Party</h1>
-        <h3>Ain't no party like an API party</h3>
+          <h3>Ain't no party like an</h3>
+          <h1>API Party</h1>
         </header>
-      </div>
-    );
+
+        <ul className="navLinks">
+          <li>
+            <NavLink to="/github">GitHub API</NavLink>
+          </li>
+        </ul>
+        <Switch>
+          <Route path="/github" render={() => <h1>GitHub!</h1>} />
+          <Route  render={() =>
+            <p>To get started, click one of the links above.</p>} />
+        </Switch>  
+      </div>  
+    )
   }
 }
 
-export default App;
+export default App
