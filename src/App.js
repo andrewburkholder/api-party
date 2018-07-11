65b490954281e990d90f6b7688ee90988ceda313
Andrew Burkholder <hi@andrew.vip>
2018-07-10 20:39:19 -0400

    Use Davey's code to add GitHub api

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react'
 import { Route, NavLink, Switch } from 'react-router-dom'
 
 import './App.css'
+import Github from './Github'
 
 class App extends Component {
   render() {
@@ -16,13 +17,18 @@ class App extends Component {
           <li>
             <NavLink to="/github">GitHub API</NavLink>
           </li>
+          <li>
+            <NavLink to="/homework">Homework</NavLink>
+          </li>
         </ul>
+
         <Switch>
-          <Route path="/github" render={() => <h1>GitHub!</h1>} />
-          <Route  render={() =>
-            <p>To get started, click one of the links above.</p>} />
-        </Switch>  
-      </div>  
+          <Route path="/github" component={Github} />
+          <Route render={() => (
+              <p>To get started, click one of the links above.</p>
+          )} />
+        </Switch>
+      </div>
     )
   }
 }
